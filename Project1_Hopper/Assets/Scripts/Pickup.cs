980a964ee7fcfba2a432a48186f67b698380112b
Bryce Monaco <Bryce@BrycBook-Pro.local>
2017-10-08 14:25:28 -0700

    Pickups Now Despawn

--- Project1_Hopper/Assets/Scripts/Pickup.cs
+++ Project1_Hopper/Assets/Scripts/Pickup.cs
@@ -17,6 +17,11 @@ public class Pickup : MonoBehaviour
 {
 	public PickupType thisType;
 
+	public int minWait = 5;
+	public int maxWait = 15;
+
+	public GameObject pickupArtObject;
+
 	void OnTriggerEnter (Collider col)
 	{
 		if (col.CompareTag("Player"))
@@ -24,6 +29,20 @@ public class Pickup : MonoBehaviour
 			col.GetComponent<PlayerControl> ().AddPickup (thisType);
 
 		}
+
+		StartCoroutine (WaitTimer ());
+
+	}
+
+	IEnumerator WaitTimer ()
+	{
+		pickupArtObject.SetActive (false);
+		GetComponent<BoxCollider> ().enabled = false;
+
+		yield return new WaitForSeconds (Random.Range (minWait, maxWait));
+
+		pickupArtObject.SetActive (true);
+		GetComponent<BoxCollider> ().enabled = true;
 
 	}
 }
