645a3ab06d83175fb64a8b4aa58186fafb3082c6
Bryce Monaco <Bryce@BrycBook-Pro.local>
2017-10-31 20:07:20 -0700

    Created Main Menu

--- Project2_Thrower/Assets/Scripts/ProjectileFollow.cs
+++ Project2_Thrower/Assets/Scripts/ProjectileFollow.cs
@@ -16,12 +16,27 @@ public class ProjectileFollow : MonoBehaviour
 	public Transform projectile;
 	public Transform farLeft;
 	public Transform farRight;
+	public Transform cameraStartPoint;
 
 	private bool shouldGoBack = false;
+	private bool waitForStart = true;
 	static private float t = 0f;
 	private float startPos;
 	public float returnSpeed = 0.5f;
-	
+
+	void Start ()
+	{
+		waitForStart = true;
+
+		Vector3 newPosition = transform.position;
+		newPosition.x = cameraStartPoint.position.x;
+
+		transform.position = newPosition;
+
+		StartCoroutine (StartWait ());
+
+	}
+
 	void Update () 
 	{
 		if (shouldGoBack)
@@ -38,9 +53,15 @@ public class ProjectileFollow : MonoBehaviour
 			{
 				shouldGoBack = false;
 
+				if (waitForStart)
+				{
+					waitForStart = false;
+
+				}
+
 			}
 
-		} else if (projectile != null)
+		} else if (projectile != null && !waitForStart)
 		{
 			Vector3 newPosition = transform.position;
 			newPosition.x = projectile.position.x;
@@ -60,6 +81,15 @@ public class ProjectileFollow : MonoBehaviour
 		t = 0f;
 
 		startPos = transform.position.x;
+
+	}
+
+	IEnumerator StartWait ()
+	{
+
+		yield return new WaitForSeconds (3f);
+
+		ReturnCamera ();
 
 	}
 
