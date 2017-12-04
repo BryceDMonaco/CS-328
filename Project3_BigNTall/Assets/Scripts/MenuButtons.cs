3a1d759d2c97a52a633c483d47dd726389fdec7a
Bryce Monaco <Bryce@BrycBook-Pro.local>
2017-12-04 14:13:20 -0800

    Added Main Menu

---
+++ Project3_BigNTall/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,28 @@
+﻿/*
+ *	Author: Bryce Monaco
+ *	Last Updated: 12/4/2017
+ *
+ *	Description:
+ *
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour 
+{
+
+	public void PlayGame ()
+	{
+		SceneManager.LoadScene ("Scene1");
+
+	}
+
+	public void ReturnToMenu ()
+	{
+		SceneManager.LoadScene ("Menu");
+
+	}
+}
